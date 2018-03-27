cc64a41703d93ab778310ff86e2b2df3aeb1b0dd
Helium <1029535012@qq.com>
2018-03-27 09:03:03 +0800

    LiveWallpaper v1.24a

---
+++ LiveWallpaperCore/SDL2.h
@@ -0,0 +1,19 @@
+/*
+*     COPYRIGHT NOTICE
+*     Copyright(c) 2017~2018, alopex
+*     All rights reserved.
+*
+* @file		SDL2.h
+* @brief	This Program is SDL Library.
+* @author	alopex
+* @version	v1.00a
+* @date		2017-11-29
+*/
+#pragma once
+
+extern "C"
+{
+#include "SDL.h"
+}
+
+#pragma comment(lib, "SDL2.lib")
